2db8bebf5d93212807b9ce6ac74c3953d907507b
lgy9071 <lgy990711@naver.com>
2025-10-01 00:11:24 +0900

    게시판 폴더 만들기 전까지

---
+++ boardTest/src/test/java/com/example/boardTest/BoardTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
