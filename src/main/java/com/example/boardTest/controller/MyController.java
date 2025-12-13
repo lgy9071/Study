a52005e4e1ecea63a585e7ca94a332bdebb6cdd2
lgy9071 <lgy990711@naver.com>
2025-12-13 17:32:24 +0900

    인덱스 이동 테스트

--- src/main/java/com/example/boardTest/controller/MyController.java
+++ src/main/java/com/example/boardTest/controller/MyController.java
@@ -1,7 +1,14 @@
 package com.example.boardTest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
+
+    @GetMapping("/")
+    public String index(){
+        return "OK";
+    }
+
 }
--- src/main/java/com/example/boardTest/repository/MyRepository.java
+++ src/main/java/com/example/boardTest/repository/MyRepository.java
@@ -1,7 +1,11 @@
 package com.example.boardTest.repository;
 
+import com.example.boardTest.dto.Student;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MyRepository {
+
 }
