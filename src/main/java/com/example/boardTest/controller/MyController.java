27918bea032f024c28bf60c9b5333220dc5e47e2
lgy9071 <lgy990711@naver.com>
2025-12-13 17:43:05 +0900

    connecting index page
    의존성 부여 누락 생성

--- src/main/java/com/example/boardTest/controller/MyController.java
+++ src/main/java/com/example/boardTest/controller/MyController.java
@@ -2,13 +2,14 @@ package com.example.boardTest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 
     @GetMapping("/")
     public String index(){
-        return "OK";
+        return "index";
     }
 
 }
